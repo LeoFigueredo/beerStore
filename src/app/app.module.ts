d4848f5c8be7465034ebbc6de328bb0d7333c235
LeoFigueredo <leonardofprada@gmail.com>
2019-03-03 16:46:40 -0400

    Upload avatar to firebase storage, progress bar charge and set image to current user.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/user/register/register.component';
@@ -13,34 +12,28 @@ import { OffersComponent } from './components/offers/offers.component';
 import { DetailBeerComponent } from './components/detail-beer/detail-beer.component';
 import { ListBeerComponent } from './components/admin/list-beer/list-beer.component';
 import { Page404Component } from './components/page404/page404.component';
-
-// services
 import { DataApiService } from './services/data-api.service';
-
-
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { FormsModule } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
-
-
-
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent,
-    ProfileComponent,
-    NavbarComponent,
-    FrontComponent,
-    HomeComponent,
-    OffersComponent,
     DetailBeerComponent,
     ListBeerComponent,
+    FrontComponent,
+    HomeComponent,
+    NavbarComponent,
+    OffersComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
     Page404Component
   ],
   imports: [
@@ -48,13 +41,10 @@ import { AngularFireAuth } from '@angular/fire/auth';
     AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule
-
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
   ],
-  providers: [
-    DataApiService,
-    AngularFireAuth
-  ],
+  providers: [AngularFireAuth, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
